af483e72f1286851465d2a742726bc786c703488
Kevin <kjs2b@virginia.edu>
2017-10-05 16:08:22 -0700

    simple placeholder SQLite code added

--- db.js
+++ db.js
@@ -1,2 +1,15 @@
 const sqlite3 = require('sqlite3').verbose();
 
+const db = new sqlite3.Database('./fsg_challenge.db');
+
+db.serialize(() => {
+  db.run("CREATE TABLE IF NOT EXISTS metrics (" +
+    "id INTEGER PRIMARY KEY, " +
+    "title VARCHAR(255))");
+});
+
+db.run("INSERT INTO metrics (id, title) VALUES (124, 'Working Title')");
+
+var results = db.all("SELECT * FROM metrics", (err, row) => {
+  console.log(row);
+});
